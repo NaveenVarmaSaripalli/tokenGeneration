cbf3e31ea038e1992eaf8cddd20489643bde635b
NaveenVarmaSaripalli <45036813+NaveenVarmaSaripalli@users.noreply.github.com>
2018-11-19 19:14:25 +0530

    Update JdbcTemplateConfigurer.java

--- src/main/java/com/tokengeneration/configurations/JdbcTemplateConfigurer.java
+++ src/main/java/com/tokengeneration/configurations/JdbcTemplateConfigurer.java
@@ -18,9 +18,9 @@ public class JdbcTemplateConfigurer {
 	public DataSource dataSource() {
 		final DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
 		driverManagerDataSource.setDriverClassName("com.mysql.jdbc.Driver");
-		driverManagerDataSource.setUrl("jdbc:mysql://localhost:3306/turvo");
+		driverManagerDataSource.setUrl("jdbc:mysql://localhost:3306/tokenGeneration");
 		driverManagerDataSource.setUsername("root");
-		driverManagerDataSource.setPassword("Nav@1437");
+		driverManagerDataSource.setPassword("root");
 		return driverManagerDataSource;
 	}
 	
